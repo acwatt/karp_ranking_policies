1855fbff8a653bed63a718f33b7bdf8bb6948d4f
AC Watt <aaron@acwatt.net>
2023-07-11 15:14:09 -0700

    Added Distributed.jl testing

--- ranking_policies/julia/KarpEstimation/Utilities/savio_test.jl
+++ ranking_policies/julia/KarpEstimation/Utilities/savio_test.jl
@@ -1,17 +1,43 @@
 # Test savio
-# import Pkg; Pkg.add("BenchmarkTools")
-using BenchmarkTools
-# Test parallel computing using Threads.@threads
-# @benchmark sort(data) setup=(data=rand(10))
 
-println("# of Threads:$(Threads.nthreads())")
 
-function parallel_test()
-    Threads.@threads for i in 1:100
-        sleep(1/100)
+using Distributed
+
+@everywhere begin
+    project_env = splitpath(@__DIR__)[1:end-1]
+    using Pkg; Pkg.activate(project_env)
+    Pkg.instantiate(); Pkg.precompile()
+end
+
+@everywhere begin
+    # import Pkg; Pkg.add("BenchmarkTools")
+    using BenchmarkTools
+    # Test parallel computing using Threads.@threads
+    # @benchmark sort(data) setup=(data=rand(10))
+
+    println("# of Threads:$(Threads.nthreads())")
+
+    function parallel_test()
+        Threads.@threads for i in 1:100
+            sleep(1/100)
+        end
     end
 end
-t = @benchmark parallel_test()
+
+function distributed_test()
+    status = @showprogress pmap(1:100) do i
+        try
+        sleep(1/100)
+        true # success
+        catch e
+        false # failure
+        end
+    end
+    return nothing
+end
+
+# t = @benchmark parallel_test()
+t = @benchmark distributed_test()
 
 display(t)
 println(mean(t))
