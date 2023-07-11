e9af8bd474ba1d4858a73792a94893169afef085
AC Watt <aaron@acwatt.net>
2023-07-11 13:29:19 -0700

    Savio - increasing time limit to 30 min

--- ranking_policies/savio-scripts/savio1.sh
+++ ranking_policies/savio-scripts/savio1.sh
@@ -3,7 +3,7 @@
 #SBATCH --account=fc_rankpolicy
 #SBATCH --partition=savio3
 #SBATCH --nodes=1
-#SBATCH --time=00:00:30
+#SBATCH --time=00:30:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=aaron.watt@berkeley.edu
 #SBATCH --output=savio-scripts/karp_sim_methods_compare_%j.out
