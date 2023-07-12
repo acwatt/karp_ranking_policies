ad18a9d1b8a43c476d4bd4b4cf0faf55736b9e9b
AC Watt <aaron@acwatt.net>
2023-07-11 19:21:22 -0700

    Adding SlurmClusterManager

--- ranking_policies/julia/KarpEstimation/Utilities/savio_test.jl
+++ ranking_policies/julia/KarpEstimation/Utilities/savio_test.jl
@@ -1,44 +1,50 @@
+#!/usr/bin/env julia
 # Test savio
 
 
-using Distributed
+# using Distributed
 
-@everywhere begin
-    project_env = joinpath(splitpath(@__DIR__)[1:end-1])
-    using Pkg; Pkg.activate(project_env)
-    Pkg.instantiate(); Pkg.precompile()
-end
+# @everywhere begin
+#     project_env = joinpath(splitpath(@__DIR__)[1:end-1])
+#     using Pkg; Pkg.activate(project_env)
+#     Pkg.instantiate(); Pkg.precompile()
+# end
 
-@everywhere begin
-    # import Pkg; Pkg.add("BenchmarkTools")
-    using BenchmarkTools
-    using ProgressMeter
-    # Test parallel computing using Threads.@threads
-    # @benchmark sort(data) setup=(data=rand(10))
+# @everywhere begin
+#     # import Pkg; Pkg.add("BenchmarkTools")
+#     using BenchmarkTools
+#     using ProgressMeter
+#     # Test parallel computing using Threads.@threads
+#     # @benchmark sort(data) setup=(data=rand(10))
 
-    println("# of Threads:$(Threads.nthreads())")
+#     println("# of Threads:$(Threads.nthreads())")
 
-    function parallel_test()
-        Threads.@threads for i in 1:100
-            sleep(1/100)
-        end
-    end
-end
+#     function parallel_test()
+#         Threads.@threads for i in 1:100
+#             sleep(1/100)
+#         end
+#     end
+# end
 
-function distributed_test()
-    status = @showprogress pmap(1:100) do i
-        try
-        sleep(1/100)
-        true # success
-        catch e
-        false # failure
-        end
-    end
-    return nothing
-end
+# function distributed_test() 
+#     status = pmap(1:100) do i
+#         try
+#             sleep(1/100)
+#             true # success
+#         catch e
+#             false # failure
+#         end
+#     end
+#     return nothing
+# end
 
 # t = @benchmark parallel_test()
-t = @benchmark distributed_test()
+# t = @benchmark distributed_test(); display(t)
 
-display(t)
-println(mean(t))
+
+
+
+
+using Distributed, SlurmClusterManager
+addprocs(SlurmManager())
+@everywhere println("hello from $(myid()):$(gethostname())")
