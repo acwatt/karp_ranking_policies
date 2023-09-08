814e70a83bcf6361d6a7dacc6c7107cb1b7361de
AC Watt <aaron@acwatt.net>
2023-09-08 10:08:53 -0700

    Adding password template

--- ranking_policies/julia/KarpEstimation/Utilities/passwords template.jl
+++ ranking_policies/julia/KarpEstimation/Utilities/passwords template.jl
@@ -1,3 +1,4 @@
 # Followed these steps to generate an app pwd for kcgc email: https://support.google.com/accounts/answer/185833?hl=en
 # password stored in last pass
 google_app_pwd = ""
+# Copy this file and rename it to passwords.jl, then fill in the password above
