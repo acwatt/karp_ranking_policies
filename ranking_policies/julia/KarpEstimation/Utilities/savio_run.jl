c61179e295c899b567e90ebea10baf455279b67f
AC Watt <aaron@acwatt.net>
2023-09-11 11:30:41 -0700

    starting to build savio run code

--- ranking_policies/julia/KarpEstimation/Utilities/savio_run.jl
+++ ranking_policies/julia/KarpEstimation/Utilities/savio_run.jl
@@ -1,38 +1,9 @@
 #!/usr/bin/env julia --threads=auto --project=./julia/KarpEstimation
-# Test savio
-
-
-# using Distributed
-
-# @everywhere begin
-#     project_env = joinpath(splitpath(@__DIR__)[1:end-1])
-#     using Pkg; Pkg.activate(project_env)
-#     Pkg.instantiate(); Pkg.precompile()
-# end
-
-# @everywhere begin
-#     # import Pkg; Pkg.add("BenchmarkTools")
-#     using BenchmarkTools
-#     using ProgressMeter
-#     # Test parallel computing using Threads.@threads
-#     # @benchmark sort(data) setup=(data=rand(10))
-
-#     println("# of Threads:$(Threads.nthreads())")
-
-#     function parallel_test()
-#         Threads.@threads for i in 1:100
-#             sleep(1/100)
-#         end
-#     end
-# end
-
-
-
-# t = @benchmark parallel_test()
-# t = @benchmark distributed_test(); display(t)
 
 using Distributed, SlurmClusterManager
-# using BenchmarkTools
+# Command below for creating the package locally, then instantiating in remote nodes
+# ]add Turing DataFrames Dates Optim ProgressMeter LoggingExtras CSV SMTPClient
+using BenchmarkTools
 addprocs(SlurmManager())
 # instantiate and precompile environment in all processes
 @everywhere begin
