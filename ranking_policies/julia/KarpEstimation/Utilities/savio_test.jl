10687ebd28b4b4c151f81a8ae47d2e085aa6bea5
AC Watt <aaron@acwatt.net>
2023-09-11 15:06:15 -0700

    Manually adding package list

--- ranking_policies/julia/KarpEstimation/Utilities/savio_test.jl
+++ ranking_policies/julia/KarpEstimation/Utilities/savio_test.jl
@@ -68,15 +68,16 @@
 
 #!/usr/bin/env julia
 using Distributed, SlurmClusterManager
-using BenchmarkTools
 addprocs(SlurmManager())
-using Pkg
-Pkg.add("SMTPClient")
+# using Pkg
+# Pkg.add("SMTPClient")
 # using Pkg; Pkg.activate(@__DIR__)
 # Pkg.instantiate(); Pkg.precompile()
 # instantiate and precompile environment in all processes
 @everywhere begin
-    using Pkg; Pkg.activate(@__DIR__)
+    using Pkg
+    Pkg.activate(@__DIR__)
+    Pkg.add(["SMTPClient", "CSV"])
     # Pkg.instantiate()
     include("Communications.jl")  # send_txt
 end
