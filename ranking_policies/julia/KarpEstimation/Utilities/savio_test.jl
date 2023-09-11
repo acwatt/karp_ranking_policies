51e4cb18a34d57a1888da302c1797eda5a12574b
AC Watt <aaron@acwatt.net>
2023-09-11 09:13:38 -0700

    trying simple savio test

--- ranking_policies/julia/KarpEstimation/Utilities/savio_test.jl
+++ ranking_policies/julia/KarpEstimation/Utilities/savio_test.jl
@@ -31,39 +31,42 @@
 # t = @benchmark parallel_test()
 # t = @benchmark distributed_test(); display(t)
 
-using Distributed, SlurmClusterManager
-# using BenchmarkTools
-addprocs(SlurmManager())
-# instantiate and precompile environment in all processes
-@everywhere begin
-    project_env = joinpath(splitpath(@__DIR__)[1:end-1])
-    println(project_env)
-    using Pkg; Pkg.activate(project_env)
-    Pkg.instantiate(); Pkg.precompile()
-end
-@everywhere begin
-    using Distributions
-end
+# using Distributed, SlurmClusterManager
+# # using BenchmarkTools
+# addprocs(SlurmManager())
+# # instantiate and precompile environment in all processes
+# @everywhere begin
+#     println(@__DIR__)
+#     using Pkg; Pkg.activate(@__DIR__)
+#     Pkg.instantiate(); Pkg.precompile()
+# end
+# @everywhere begin
+#     using Distributions
+# end
 
-function distributed_test() 
-    pmap(1:100) do i
-        try
-            sleep(1/100)
-            true # success
-        catch e
-            false # failure
-        end
-    end
-    return nothing
-end
+# function distributed_test() 
+#     pmap(1:100) do i
+#         try
+#             sleep(1/100)
+#             true # success
+#         catch e
+#             false # failure
+#         end
+#     end
+#     return nothing
+# end
 
-println("# of Threads:$(Threads.nthreads())")
-println("# of workers:$(nprocs())")
-@info "# of Threads:$(Threads.nthreads())"
-@info "# of workers:$(nprocs())"
+# println("# of Threads:$(Threads.nthreads())")
+# println("# of workers:$(nprocs())")
+# @info "# of Threads:$(Threads.nthreads())"
+# @info "# of workers:$(nprocs())"
+
 # t = @benchmark distributed_test(); display(t)
 # @everywhere println("hello from $(myid()):$(gethostname()) with $(Threads.nthreads()) threads")
 
-
-
 # include("../KarpEstimation.jl")
+
+#!/usr/bin/env julia
+using Distributed, SlurmClusterManager
+addprocs(SlurmManager())
+@everywhere println("hello from $(myid()):$(gethostname())")
