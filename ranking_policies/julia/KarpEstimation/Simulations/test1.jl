7f8803944b8406b53bf271de49e658f6eeb06c2b
AC Watt <aaron@acwatt.net>
2023-09-11 23:01:12 -0700

    Adding Turing to base env

--- ranking_policies/julia/KarpEstimation/Simulations/test1.jl
+++ ranking_policies/julia/KarpEstimation/Simulations/test1.jl
@@ -6,8 +6,9 @@ println(Pkg.project().path)
 #! precompile here
 using Distributed, SlurmClusterManager
 addprocs(SlurmManager())  # this will only work when run on Savio
-using Turing
 
+Pkg.add("Turing")
+using Turing
 @info "using Turing in global"
 
 @everywhere using Turing
