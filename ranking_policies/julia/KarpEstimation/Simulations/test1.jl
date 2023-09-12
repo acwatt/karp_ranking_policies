e0d339596c11adf620503a5f62c20e91bed7e6ab
AC Watt <aaron@acwatt.net>
2023-09-11 21:38:50 -0700

    importing pkg

--- ranking_policies/julia/KarpEstimation/Simulations/test1.jl
+++ ranking_policies/julia/KarpEstimation/Simulations/test1.jl
@@ -1,6 +1,7 @@
 # This file is designed to create highly parallel simuluations and estimation of the model using Turing.
 # For distriubted computing on Savio cluster
 #! remove all in .julia/compiled/v1.9/
+using Pkg
 println(Pkg.project().path)
 #! precompile here
 using Distributed, SlurmClusterManager
