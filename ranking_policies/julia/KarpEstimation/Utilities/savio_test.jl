a2415902e04ade25894bbeb1cda2d6405ed1f436
AC Watt <aaron@acwatt.net>
2023-09-11 18:28:33 -0700

    test modifying dataframe from global scope

--- ranking_policies/julia/KarpEstimation/Utilities/savio_test.jl
+++ ranking_policies/julia/KarpEstimation/Utilities/savio_test.jl
@@ -9,7 +9,7 @@ try; rm(string(@__DIR__, "/Project.toml")); catch e; end
 # Resetup the environment files
 using Pkg
 Pkg.activate(@__DIR__)
-Pkg.add(["SMTPClient", "CSV", "Turing", "Optim", "DynamicHMC", "Bijectors"])
+Pkg.add(["SMTPClient", "CSV", "Turing", "Optim", "DynamicHMC", "Bijectors", "DataFrames"])
 @info "Done creating new environment."
 
 # instantiate/add environment in all processes
@@ -23,17 +23,22 @@ end
 
 # Wait until all packages have been added, then load needed packages
 # Include files in global scope first, then everywhere according to https://github.com/JuliaLang/julia/issues/16788#issuecomment-226977022
+using DataFrames
 include("Communications.jl")  # send_txt
 @everywhere begin
+    using DataFrames
     include("Communications.jl")  # send_txt
 end
 send_txt("savio_test start", "")
 @info "Done with package loads."
 
+
 # Test distributed computing and txting updates
-function distributed_test() 
+function distributed_test!(df) 
     pmap(1:100) do i
         try
+            println("a = ", df[i, :a], ", b = ", df[i, :b], ", i = ", i)
+            df[i, :c] = df[i, :a] + df[i, :b]
             sleep(1/100)
             true # success
         catch e
@@ -44,7 +49,10 @@ function distributed_test()
     return nothing
 end
 # t = @benchmark distributed_test(); display(t)
-distributed_test()
+df = DataFrame(a = 1:100, b = 101:200, c = 201:300)
+distributed_test!(df)
+@show df
+
 @info "Done with distributed_test."
 # send_txt("savio_test end", "")
 
